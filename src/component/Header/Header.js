1f0a8a6e90a8bf8e26e856b2629f5f58d6eb1b29
Farhadcode <farhad.ahmed.aust@gmail.com>
2021-09-23 16:42:06 +0600

    ema-jhon code addad

---
+++ src/component/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../../images/logo.png'
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Header is comeing</h1>
+            <img src={logo} alt="" />
+        </div>
+    );
+};
+
+export default Header;
