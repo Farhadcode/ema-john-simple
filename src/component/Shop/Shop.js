16f26005d0f4398bfda87eb8b287e00ff2ecfb42
Farhadcode <farhad.ahmed.aust@gmail.com>
2021-10-02 12:24:40 +0600

    netlify problem solve

--- src/component/Shop/Shop.js
+++ src/component/Shop/Shop.js
@@ -47,7 +47,18 @@ const Shop = () => {
     }, [products])
 
     const handleAddToCart = (product) => {
-        const newCart = [...cart, product];
+        const exists = cart.find(pd => pd.key === product.key);
+        let newCart = [];
+        if (exists) {
+            const rest = cart.filter(pd => pd.key !== product.key);
+            exists.quantity += 1;
+            newCart = [...rest, product];
+        }
+        else {
+            product.quantity = 1;
+            newCart = [...cart, product];
+        }
+        // const newCart = [...cart, product];
         setCart(newCart);
         //console.log(product.key);
 
